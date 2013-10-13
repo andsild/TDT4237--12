8feda008de5366c904443f15e687f67b50e5faa3
Filip Egge <filip.egge@gmail.com>
2013-10-13 12:29:20 +0200

    email removed from customersupport

--- bookstore/src/amu/action/CustomerSupportAction.java
+++ bookstore/src/amu/action/CustomerSupportAction.java
@@ -1,7 +1,9 @@
 package amu.action;
 
+import amu.Config;
 import amu.Mailer;
 import amu.model.Customer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -12,15 +14,24 @@ class CustomerSupportAction implements Action {
     public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         HttpSession session = request.getSession(true);
         Customer customer = (Customer) session.getAttribute("customer");
+        String email;
 
         if (customer == null) {
             ActionResponse actionResponse = new ActionResponse(ActionResponseType.REDIRECT, "loginCustomer");
             actionResponse.addParameter("from", "customerSupport");
             return actionResponse;
         }
-
+        if (request.getParameter("department") == "sales") {
+			email = Config.EMAIL_SALES;
+		}
+        else if (request.getParameter("department")== "techSupport") {
+			email = Config.EMAIL_TECH;
+		}
+        else{
+        	email = Config.EMAIL_FROM_ADDR;
+        }
         if (request.getMethod().equals("POST")) {
-            Mailer.send(request.getParameter("department"), 
+            Mailer.send(email, 
                     request.getParameter("subject"), 
                     request.getParameter("content"), 
                     request.getParameter("fromAddr"), 
