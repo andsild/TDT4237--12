0bbaef04a89c4ec6cba9a0979edd4ea9e5714fa9
kwrl <kitteeeeeeh@gmail.com>
2013-10-24 18:56:02 +0200

    ActivateCustomerAction improved.

--- bookstore/src/amu/action/ActivateCustomerAction.java
+++ bookstore/src/amu/action/ActivateCustomerAction.java
@@ -1,36 +1,57 @@
 package amu.action;
 
-import amu.database.CustomerDAO;
-import amu.model.Customer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-class ActivateCustomerAction implements Action {
+import amu.Config;
+import amu.FilterUnitException;
+import amu.database.CustomerDAO;
+import amu.model.Customer;
 
-    @Override
-    public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+class ActivateCustomerAction implements Action
+{
 
-        // If email field is set, but token is not provided then this is a idempotent request
-        if (request.getParameter("email") != null && request.getParameter("activationToken") == null) {
-            request.setAttribute("email", request.getParameter("email"));
-        }
+	@Override
+	public ActionResponse execute(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		String email, activationToken;
 
-        // If both fields are set, then this is a non-idempotent activation request
-        if (request.getParameter("email") != null && request.getParameter("activationToken") != null) {
-            CustomerDAO customerDAO = new CustomerDAO();
-            Customer customer = customerDAO.findByEmail(request.getParameter("email"));
+		email = request.getParameter("email");
+		activationToken = request.getParameter("activationToken");
 
-            if (customer != null && customer.getActivationToken().equals(request.getParameter("activationToken"))) {
-                customer = customerDAO.activate(customer);
-                return new ActionResponse(ActionResponseType.REDIRECT, "activationSuccessful");
-            } else {
-                HttpSession session = request.getSession(true);
-                session.setAttribute("debugActivation", customer);
-                return new ActionResponse(ActionResponseType.REDIRECT, "activationError");
-            }
-        }
-        
-        return new ActionResponse(ActionResponseType.FORWARD, "activateCustomer");
-    }
+		if (email == null)
+			return new ActionResponse(ActionResponseType.FORWARD, "activateCustomer");
+
+		if (activationToken == null)
+			request.setAttribute("email", email);
+		else
+		{
+			try
+			{
+				Config.VALIDATE_EMAIL.isValid(email);
+				Config.VALIDATE_TEXT_AND_NUMBERS.isValid(activationToken);
+			} catch (FilterUnitException e)
+			{
+				return new ActionResponse(ActionResponseType.REDIRECT, "activationError");
+			}
+
+			CustomerDAO customerDAO = new CustomerDAO();
+			Customer customer = customerDAO.findByEmail(email);
+
+			if (customer != null && customer.getActivationToken().equals(activationToken))
+			{
+				customer = customerDAO.activate(customer);
+				return new ActionResponse(ActionResponseType.REDIRECT, "activationSuccessful");
+			}
+			else
+			{
+				HttpSession session = request.getSession(true);
+				session.setAttribute("debugActivation", customer);
+				return new ActionResponse(ActionResponseType.REDIRECT, "activationError");
+			}
+		}
+
+		return new ActionResponse(ActionResponseType.FORWARD, "activateCustomer");
+	}
 }
